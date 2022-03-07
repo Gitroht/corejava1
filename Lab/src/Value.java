de5e812649962b148576b3b8e5e2f54d75ee5a0a
shambusen <shambusen@shambus-air>
2022-03-07 10:43:36 +0600

    Fifth Commit

---
+++ Lab/src/Value.java
@@ -0,0 +1,18 @@
+
+public class Value {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int x=7;
+       if(x!=5)
+       {
+    	   System.out.println("value of x is not 5");
+    	   
+       }
+       else
+       {
+    	   System.out.println("value of x is 5");
+       }
+	}
+
+}
