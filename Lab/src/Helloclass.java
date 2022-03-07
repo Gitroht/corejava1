4ab7c1e9513a92f55d636791bf4fd94eeebc4c92
Rohit <shambusen@shambus-air>
2022-03-07 10:37:37 +0600

    Fourth Commit

---
+++ Lab/src/Helloclass.java
@@ -0,0 +1,20 @@
+
+public class Helloclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=8,y=10;
+		if(x>=y)
+		{
+			System.out.println("true");
+			
+		}
+		else
+		{
+			System.out.println("false");
+			
+		}
+
+	}
+
+}
