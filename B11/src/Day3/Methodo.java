c74858d9eed9229a807c5bc4e00c6f29aca1302d
Rohit <shambusen@shambus-air>
2022-03-11 15:34:40 +0600

    Day 3 Commit

---
+++ B11/src/Day3/Methodo.java
@@ -0,0 +1,21 @@
+package Day3;
+
+public class Methodo {
+	static int add(int a, int b)
+	{
+		return a+b;
+	}
+	static double add(double a,double b)
+	{
+		return a+b;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add (2, 5));
+		
+		System.out.println(add (4.5,5.8));
+	}
+
+}
