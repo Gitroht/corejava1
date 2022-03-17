a0e98c72334217d8aa4afcbf4561ecf749e36675
Rohit <shambusen@shambus-air>
2022-03-17 21:47:42 +0600

    Fact day7

---
+++ B11/src/day7/Fact.java
@@ -0,0 +1,20 @@
+package day7;
+
+public class Fact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=8;
+		int fact=1,i;
+		for(i=2;i<=n;i++)
+		{
+			fact=fact * i;
+			
+			
+		}
+		System.out.println(fact);
+		
+
+	}
+
+}
