6192d9f72832798774d8d83d0fd71d0963ef875e
ROHIT <ROHIT@LAPTOP-2HU4LRKH>
2022-03-26 23:16:57 +0530

    day12 leap year

---
+++ Practical/src/day12/leap.java
@@ -0,0 +1,19 @@
+package day12;
+
+public class leap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year=2022;
+		if(year%4==0)
+		{
+			System.out.println("leap year");
+			
+		}
+		else
+		{
+			System.out.println("not leap year");
+		}
+	}
+
+}
