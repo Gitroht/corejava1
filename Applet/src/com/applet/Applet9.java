6ff7ab793663310dd5661444e9d493f321044747
Rohit <USER@USER-PC>
2022-04-04 15:45:40 +0530

    table 1 to 10

---
+++ Applet/src/com/applet/Applet9.java
@@ -0,0 +1,31 @@
+package com.applet;
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Applet9 extends Applet {
+	public void paint(Graphics g) {
+		setBackground(Color.pink);
+		setForeground(Color.red);
+		
+		
+		int x=20, y=20;
+		for(int i=1;i<10;i++)
+		{
+			for(int j=1;j<=10;j++)
+			
+				
+			{
+				int k=i+j;
+				g.drawString(k+"", x, y);
+				x=x+30;
+			}
+			x=20;
+			y=y+30;
+			
+			}
+		}
+	}
+
+
