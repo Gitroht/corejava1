b8c7961f93e2bc171a198dc8f7acbb48f39a43b4
Rohit <USER@USER-PC>
2022-04-07 15:59:41 +0530

    applet part 3

---
+++ Applet/src/com/applet/Applet2part3.java
@@ -0,0 +1,43 @@
+package com.applet;
+
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Applet2part3 extends Applet implements ActionListener  {
+	Button b1,b2;
+	TextField t;
+	public void init()
+	{
+		add(b1=new Button("Click"));
+		add(t=new TextField(15));
+		add(b2=new Button("Clear"));
+		b1.addActionListener(this);
+		b2.addActionListener(new A());
+		
+	}
+	public void actionPerformed(ActionEvent arg0) {
+		t.setText("Hello World");
+		}
+class A implements ActionListener{
+
+	
+		public void actionPerformed(ActionEvent e) {
+		
+	t.setText("");
+	
+	
+}
+}
+
+	
+	
+	
+	
+	
+	
+	
+}
+
