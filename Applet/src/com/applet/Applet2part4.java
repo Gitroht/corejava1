d9b8eac71c44fd82883c70bd72ea5cfdbaeb9526
Rohit <USER@USER-PC>
2022-04-07 16:00:32 +0530

    applet part4

---
+++ Applet/src/com/applet/Applet2part4.java
@@ -0,0 +1,36 @@
+package com.applet;
+
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Applet2part4 extends Applet{
+Button b1,b2;
+TextField t;
+public void init() {
+	add(b1=new Button("Click"));
+	add(t=new TextField(15));
+	add(b2=new Button ("Clear"));
+	b1.addActionListener(new A());
+	b2.addActionListener(new B());
+	
+}
+class A implements  ActionListener{
+	public void actionPerformed(ActionEvent e) {
+		t.setText("Helo World");
+		
+	}
+}
+class B implements ActionListener {
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		t.setText("");
+		
+		
+	}
+	
+}
+}
