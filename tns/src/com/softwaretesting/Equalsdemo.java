5ed3c9eaec59d123873b485f5a6f0fe16803f289
Rohit <USER@USER-PC>
2022-04-11 00:51:49 +0530

    Equal commit

---
+++ tns/src/com/softwaretesting/Equalsdemo.java
@@ -0,0 +1,29 @@
+package com.softwaretesting;
+
+public class Equalsdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int  val1=7;
+int val2=-3;
+int val3=7;
+if(val1==val2)
+{
+	System.out.println("val1 and val2 are equal");
+	
+}
+else {
+	System.out.println("val1 and val2 are not equal");
+}
+if(val3==val2)
+{
+	System.out.println("val1 and val3 are equal");
+	
+}
+else {
+	System.out.println("val1 and val3 are not equal");
+	
+}
+	}
+
+}
