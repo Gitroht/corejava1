4928fdc8d57b0c5c4d4e1d86643cbc79932bdb0d
USER <USER@USER-PC>
2022-05-02 10:06:46 +0530

    Vec

---
+++ tns/src/empldemo/MyVector1.java
@@ -0,0 +1,18 @@
+package empldemo;
+
+class MyVector{
+	
+}
+
+public class MyVector1 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+MyVector v1,v2;
+v1= new MyVector();
+v2=v1;
+	}
+}
+
+
