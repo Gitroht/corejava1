e29198d02d3ead1393c3a06609a89b1e4784adb9
Rohit <USER@USER-PC>
2022-05-03 23:17:12 +0530

    Vector

---
+++ tns/src/empldemo/Sys.java
@@ -0,0 +1,19 @@
+package empldemo;
+import java.util.*;
+
+public class Sys {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Vector vec = new Vector();
+vec.addElement(1);
+vec.addElement(2);
+vec.addElement("Hello");
+
+System.out.println(vec);
+System.out.println(vec.get(0));
+System.out.println(vec.get(1));
+
+	}
+
+}
