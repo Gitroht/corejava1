30d602c70145c20afc572d95880c56f24f39ca77
USER <USER@USER-PC>
2022-05-23 01:20:08 +0530

    test

---
+++ tns/src/com/guiapps/Scannertest.java
@@ -0,0 +1,15 @@
+package com.guiapps;
+
+import java.util.Scanner;
+
+public class Scannertest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scanner= new Scanner(System.in);
+System.out.println("enter a character");
+char c= scanner.next().charAt(0);
+System.out.println("value ="+c);
+	}
+
+}
